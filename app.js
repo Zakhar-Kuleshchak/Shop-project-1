ecbce2130234762f025a536ab58f2310d5051954
Zakhar <zahkul94@gmail.com>
2023-01-21 22:08:17 +0200

    add counter for 1 good and for all goods

--- app.js
+++ app.js
@@ -47,7 +47,7 @@ moreDetailsBtns.forEach((item) =>
     modal.classList.add("show");
     modal.classList.remove("hide");
     modal.style.display = "block";
-    modal.style.backgroundColor = "red";
+    // modal.style.backgroundColor = "red";
   })
 );
 // закриє модальне вікно при натисканні на хрестик в модалці
@@ -99,18 +99,182 @@ window.addEventListener("scroll", showModalByScroll);
 //   })
 // );
 // Варіант 2:
+let likeBtns = document.querySelectorAll(".like");
+likeBtns.forEach((item) =>
+  item.addEventListener("click", function () {
+    item.classList.toggle("liked");
+  })
+);
+
+// Варіант 1: задасть при кліку червоний бекграунд:
 // let likeBtns = document.querySelectorAll(".like");
 // likeBtns.forEach((item) =>
 //   item.addEventListener("click", function () {
-//     item.classList.toggle("liked");
+//     item.style.backgroundColor = "red";
 //   })
 // );
-// Варіант 3:
+// Варіант 2: задасть при кліку червоний бекграунд:
 // let likeBtns = document.querySelectorAll(".like");
 // likeBtns.forEach((item) =>
-//   item.addEventListener("click", function () {
-//     item.style.background-color = red;
+//   item.addEventListener("click", function (e) {
+//     e.target.style.backgroundColor = "red";
 //   })
 // );
 
-$(".slider").slick();
+// slider
+$(".slider").slick({
+  autoplay: true,
+  dots: true,
+});
+
+// Урок 20
+// function Car(model, year, color) {
+//   this.model = model;
+//   this.year = year;
+//   this.color = color;
+//   // console.log(this);
+//   this.hi = function () {
+//     console.log("Hi " + this.model);
+//   };
+
+//   this.hello = function () {
+//     console.log("Hello " + this.model);
+//   };
+// }
+
+// let audi = new Car("A6", 2020, "black");
+// let bmw = new Car("328", 2010, "grey");
+// let mercedes = new Car("c300", 2000, "white");
+
+// console.log(audi);
+// console.log(bmw);
+// console.log(mercedes);
+
+// audi.hi();
+// mercedes.hello();
+
+// лічильник для 1го товару:
+
+// let decrementBtns = document.querySelectorAll(".decrement-btn")[0];
+// let incrementBtns = document.querySelectorAll(".increment-btn")[0];
+// let productsCount = document.querySelectorAll(".product-quantity input")[0];
+
+// function Counter(incrementBtn, decrementBtn, inputField) {
+//   this.domRefs = {
+//     incrementBtn,
+//     decrementBtn,
+//     inputField,
+//   };
+
+//   this.toggleButtonState = function () {
+//     let count = this.domRefs.inputField.value;
+//     this.domRefs.decrementBtn.disabled = count <= 1;
+//     this.domRefs.incrementBtn.disabled = count >= 10;
+//   };
+
+//   this.toggleButtonState();
+
+//   this.increment = function () {
+//     this.domRefs.inputField.value = +this.domRefs.inputField.value + 1;
+//     this.toggleButtonState();
+//   };
+
+//   this.decrement = function () {
+//     this.domRefs.inputField.value = +this.domRefs.inputField.value - 1;
+//     this.toggleButtonState();
+//   };
+
+//   this.domRefs.incrementBtn.addEventListener(
+//     "click",
+//     this.increment.bind(this)
+//   );
+//   this.domRefs.decrementBtn.addEventListener(
+//     "click",
+//     this.decrement.bind(this)
+//   );
+// }
+
+// const counter1 = new Counter(incrementBtns, decrementBtns, productsCount);
+// console.log(counter1);
+// приклад
+// let user = {
+//   name: "Pablo",
+//   hi: function () {
+//     console.log(this.name);
+//   },
+// };
+// user.hi();
+// приклад
+// function hi() {
+//   console.log(this);
+//   console.log("Hi " + this.name);
+// }
+// let pablo = {
+//   name: "pablo",
+// };
+// let anna = {
+//   name: "anna",
+// };
+// hi.apply(pablo);
+// hi.apply(anna);
+// приклад
+// function hi(surname) {
+//   console.log(this);
+//   console.log("Hi " + this.name + " " + surname);
+// }
+// let pablo = {
+//   name: "pablo",
+// };
+// let anna = {
+//   name: "anna",
+// };
+// hi.call(pablo, "Call");
+// hi.apply(anna, ["Apply"]);
+// let test = hi.bind(pablo, "Bind");
+// test();
+
+// урок 21 робимо каунтер для всіх товарів
+let decrementBtns = document.querySelectorAll(".decrement-btn");
+let incrementBtns = document.querySelectorAll(".increment-btn");
+let productsCount = document.querySelectorAll(".product-quantity input");
+
+function Counter(incrementBtn, decrementBtn, inputField) {
+  this.domRefs = {
+    incrementBtn,
+    decrementBtn,
+    inputField,
+  };
+
+  this.toggleButtonState = function () {
+    let count = this.domRefs.inputField.value;
+    this.domRefs.decrementBtn.disabled = count <= 1;
+    this.domRefs.incrementBtn.disabled = count >= 10;
+  };
+
+  this.toggleButtonState();
+
+  this.increment = function () {
+    this.domRefs.inputField.value = +this.domRefs.inputField.value + 1;
+    this.toggleButtonState();
+  };
+
+  this.decrement = function () {
+    this.domRefs.inputField.value = +this.domRefs.inputField.value - 1;
+    this.toggleButtonState();
+  };
+
+  this.domRefs.incrementBtn.addEventListener(
+    "click",
+    this.increment.bind(this)
+  );
+  this.domRefs.decrementBtn.addEventListener(
+    "click",
+    this.decrement.bind(this)
+  );
+}
+
+let counters = [];
+productsCount.forEach(
+  (item, i) =>
+    (counters[i] = new Counter(incrementBtns[i], decrementBtns[i], item))
+);
